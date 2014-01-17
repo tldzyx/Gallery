74d149e000efc76bdc7faa0d6d77f03007139a8d
tldzyx <tldzyx@gmail.com>
2014-01-17 12:08:44 +0800

    新增控件拓展, 用于切换子控件时对被移除的旧控件执行销毁操作

---
+++ Gallery/Extension/ControlExtension.cs
@@ -0,0 +1,39 @@
+﻿
+
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Gallery.Extension
+{
+    /// <summary>
+    /// 控件拓展
+    /// </summary>
+    public static class ControlExtension
+    {
+        /// <summary>
+        /// 销毁原子控件集合, 并用新的子控件填充.
+        /// </summary>
+        /// <param name="controlCollection">原子控件集合</param>
+        /// <param name="control">新的子控件</param>
+        public static void Replace(this Control.ControlCollection controlCollection, Control control)
+        {
+            controlCollection.Replace(new[] {control});
+        }
+
+        /// <summary>
+        /// 销毁原子控件集合, 并用新的子控件集合填充.
+        /// </summary>
+        /// <param name="controlCollection">原子控件集合</param>
+        /// <param name="controls">新的子控件集合</param>
+        public static void Replace(this Control.ControlCollection controlCollection, Control[] controls)
+        {
+            foreach (var item in controlCollection.OfType<IDisposable>())
+            {
+                item.Dispose();
+            }
+            controlCollection.Clear();
+            controlCollection.AddRange(controls);
+        }
+    }
+}
